cda1d3622877d091f07d809f620fabb77db650e2
JunJaeIra <eowkd300@gmail.com>
2020-07-17 20:18:38 +0900

    Practice If-else statements

--- Project1/main.c
+++ Project1/main.c
@@ -1,14 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+#define TO_STR(expr) (#expr) 
+
 int main(void)
 {
-	int n;
+	int a;
+	int b;
+	int c;
 
-	fputs("Enter the number: ", stdout);
-	scanf("%d", &n);
+	fputs("Enter the numbers: ", stdout);
+	scanf("%d %d %d", &a, &b, &c);
 
-	printf("Output: %d\n", n);
+	if (a > b)
+	{
+		if (a > c) 
+		{
+			printf("Max is %s (%d).\n", TO_STR(a), a);
+		}
+		else 
+		{
+			printf("Max is %s (%d).\n", TO_STR(c), c);
+		}
+	}
+	else 
+	{
+		if (b > c) 
+		{
+			printf("Max is %s (%d).\n", TO_STR(b), b);
+		}
+		else 
+		{
+			printf("Max is %s (%d).\n", TO_STR(c), c);
+		}
+	}
+
 
 	return 0;
 }
